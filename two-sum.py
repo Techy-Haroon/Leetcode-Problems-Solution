8a9036649f9affcef6a06526613b260300ca4430
Techy-Haroon <167862025+Techy-Haroon@users.noreply.github.com>
2024-06-28 18:08:32 +0500

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -3,7 +3,9 @@
 def twoSum(nums, target):
         for i in range(len(nums)):
             for b in range(i+1, len(nums)):
-                if((nums[i]+nums[b]) == target):
+                if((target-nums[i]) == nums[b]):
+                # Another Approach which is more easy to understand is:
+                # if((nums[i]+nums[b])==target):
                     return [i, b]
 nums = [2,4,5,6,8] # You can adjust it
 target = 9 # You should write something which your two values from num can add to form
